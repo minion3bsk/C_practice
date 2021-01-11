60796e27dbdcf37d99e9f0353cea36b878dd48bb
Mao <chocolate@Mao-MacBook-Pro.local>
2021-01-11 13:55:54 +0900

    complete escape symble

---
+++ characters.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    printf("%d\t+", 100);
+    printf("\t%d", 20);
+    printf("\t=\t%d\n", 120);
+
+
+    printf("%d\t+\t%d\t=\t%d\n",100,20,120);
+    return 0;
+}
